b818f00f93fde0f0a815966a3c1f96ab8073c1e7
matteaut <matteaum@educ.college-mnotreal.qc.ca>
2023-11-08 10:44:34 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+# Tristan Matteau
+# TP3
+
+import random
+
+result_dice1 = random.randint(1, 6)
+result_dice2 = random.randint(1, 6)
+
+def victoires():
+    victoires == 0
+    if combat1==victoires
+        victoires+1
+
+    else:
+        victoires+0
+
+def force_de_base():
+    force_de_base==2
+
+def nombre_de_victoire():
+    print("victoires")
+
+def nombre_de_defaite():
+    print("defaites")
+
+def numero_de_combat():
+    print("nombre_de_victoire vs nombre_de_defaite")
+
+def force_de_monstre1():
+    force_de_monstre1 == random.randint(1, 5)
+
+def monstre1():
+    monstre1
+
+def debut_du_jeu():
+    debut_du_jeu == monstre1
+
+def niveau_de_vie():
+    niveau_de_vie == 20
+
+def SelectionPorte():
+    print("choissez entre porte1 , porte2 , porte3 .")
+
+def combat1():
+    if force_de_base + result_dice1 + result_dice2 > force_de_monstre1:
+        print("Victoires")
+        print("Nombre de victoires")
+
+def porte1():
+
+def porte2():
+
+def porte3():
+
+
+
+print("adversaire de difficulter de force force1 est apparue.")
+
+
+
