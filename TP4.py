af46eb023ce29140d24cd99446cfeeebd6fd9dcf
matteaut <matteaum@educ.college-mnotreal.qc.ca>
2024-01-19 15:17:29 -0500

    gjcghfkxfyykicuk

--- TP4.py
+++ TP4.py
@@ -1,36 +1,55 @@
 import arcade
-
-SCREEN_WIDTH = 640
-SCREEN_HEIGHT = 480
-
-import arcade
-
+import random
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+COLORS = ["red", "blue", "green", "yellow", "purple", "orange"]  # Ajoutez plus de couleurs au besoin
 
-COLORS = []
+class Circle:
+    def __init__(self, x, y, radius, color):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
 
+    def draw(self):
+        arcade.draw_circle_filled(self.x, self.y, self.radius, arcade.color(self.color))
 
 class MyGame(arcade.Window):
-   def __init__(self):
-      super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Exercice #1")
-      pass
+    def __init__(self, width, height):
+        super().__init__(width, height, "Circles Game")
+        self.circle_list = []
 
-   def setup(self):
-      pass
+    def setup(self):
+        for _ in range(20):
+            x = random.randrange(self.width)
+            y = random.randrange(self.height)
+            radius = 30
+            color = random.choice(COLORS)
+            circle = Circle(x, y, radius, color)
+            self.circle_list.append(circle)
 
-   def on_draw(self):
-      arcade.start_render()
+    def on_draw(self):
+        arcade.start_render()
+        for circle in self.circle_list:
+            circle.draw()
 
-      pass
-
+    def on_mouse_press(self, x, y, button, modifiers):
+        if button == arcade.MOUSE_BUTTON_LEFT:
+            for circle in self.circle_list:
+                if arcade.check_for_collision((x, y), (circle.x, circle.y, circle.radius)):
+                    self.circle_list.remove(circle)
+                    break
+        elif button == arcade.MOUSE_BUTTON_RIGHT:
+            for circle in self.circle_list:
+                if arcade.check_for_collision((x, y), (circle.x, circle.y, circle.radius)):
+                    circle.color = random.choice(COLORS)
+                    break
 
 def main():
-   my_game = MyGame()
-   my_game.setup()
+    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
+    window.setup()
+    arcade.run()
 
-   arcade.run()
-
-
-main()
+if __name__ == "__main__":
+    main()
